3c6fcaaa1ed3618fa6d260dfd9eee2c0aa37992e
Shubham Singh <128978409+aryansingh822002@users.noreply.github.com>
2025-07-08 17:08:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 //complete this code
 class Person {
 	constructor(name,age){
-		this.name=name;
-		this.age=age;
+		this._name=name;
+		this._age=age;
 	}
 	get name(){
-		return this.name;
-	}
-	set name(name){
-		this.name=name;
+		return this._name;
 	}
 	get age(){
-		return this.age;
+		return this._age;
 	}
 
 	set age(age){
-		this.age=age;
+		this._age=age;
 	}
 }
 
